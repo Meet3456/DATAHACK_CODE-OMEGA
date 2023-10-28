af6d110935550621ed9fb666b3b4b47001d609ec
Meet3456 <meetvasa09@gmail.com>
2023-10-28 21:09:55 +0530

    routing added for signup

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,21 +1,21 @@
-import Landing from './Components/Landing';
-import Apps from './views/Apps';
-import Chat from './views/chat';
-import Login from './views/Login';
-import SignUp from './views/SignUp';
-import { BrowserRouter, Route,Routes} from 'react-router-dom';
+import Landing from "./Components/Landing";
+import Apps from "./views/Apps";
+import Chat from "./views/chat";
+import Login from "./views/Login";
+import SignUp from "./views/SignUp";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-      <Route path='/'element={<Landing/>}/>
-      <Route path='/apps'element={<Apps/>}/> 
-      <Route path='/chat'element={<Chat/>}/>
-      <Route path='/login'element={<Login/>}/>   
-      <Route path='/signup'element={<SignUp/>}/>  
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/apps' element={<Apps />} />
+          <Route path='/chat' element={<Chat />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
